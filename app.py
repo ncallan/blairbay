2723d726c6522ae97af499f656360f5154046a8c
ncallan <55205605+ncallan@users.noreply.github.com>
2019-11-20 19:00:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return """<h1 style="color: #256;">Hi mom</h1>"""
+
+@app.route("/hi/<name>")
+def hi(name):
+  return """<h1 style="color: #256;">Hi """ + name + """</h1>"""
